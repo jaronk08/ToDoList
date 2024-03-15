ee0b417c3e3902fc5ac2283a2db3f72f87d7149f
Jaron Kelly <jaronkellyleague@hotmail.com>
2024-03-15 14:47:59 -0400

    Now connected to SQL database. Updated/reworked button code for new database. Reorganized code for easier understanding.

--- Form1.cs
+++ Form1.cs
@@ -10,70 +10,156 @@ using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
 
+using System.Data.SqlClient; //SQL Server local db
+
 namespace ToDoList
 {
     public partial class ToDoListForm : Form
     {
+        //table to hold data of tasks
+        DataTable todoList = new DataTable();
+
+        #region Form
         public ToDoListForm()
         {
             InitializeComponent();
         }
 
-        //table to hold data of tasks
-        DataTable todoList = new DataTable();
-
-        //variable to determine is data is being edited
-        bool editing = false;
-        
-
         private void ToDoListForm_Load(object sender, EventArgs e)
         {
-            //create columns of DataTable
-            todoList.Columns.Add("Title");
-            todoList.Columns.Add("Description");
-
-            //set datagridview to todoList
-            todolistView.DataSource = todoList;
-
+            load_Tasks();
         }
+        #endregion /Form
 
+        #region Buttons
         private void editButton_Click(object sender, EventArgs e)
         {
-            editing = true;
-            //load text from selected cell and index, display it back into textboxes
-            titleTextBox.Text = todoList.Rows[todolistView.CurrentCell.RowIndex].ItemArray[0].ToString();
-            descriptionTextBox.Text = todoList.Rows[todolistView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            edit_Task();
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            if (editing)
-            {
-                todoList.Rows[todolistView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
-                todoList.Rows[todolistView.CurrentCell.RowIndex]["Description"] = descriptionTextBox.Text;
-            }
-            else
-            {
-                todoList.Rows.Add(titleTextBox.Text,descriptionTextBox.Text);
-            }
-            //clear fields
-            titleTextBox.Text = "";
-            descriptionTextBox.Text = "";
-            editing = false;
+            add_Task();
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            //delete selected row
-            try
-            {
-                todoList.Rows[todolistView.CurrentCell.RowIndex].Delete();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error: " + ex);
-            }
+            delete_Task();
         }
+        #endregion /Buttons
+
+        #region Methods
+        private void load_Tasks()
+        {
+            //connection string
+            string cn_string = Properties.Settings.Default.dbTasksConnectionString;
+
+            //connection
+            SqlConnection cn_connection = new SqlConnection(cn_string);
+
+            if (cn_connection.State != ConnectionState.Open)
+                cn_connection.Open();
+
+            string sql_Text = "SELECT * FROM tbl_Tasks";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(sql_Text, cn_connection);
+            adapter.Fill(todoList);
+
+            //set datagridview to todoList
+            todolistView.DataSource = todoList;
+
+            cn_connection.Close();
+
+        }
+
+        private void add_Task()
+        {
+            //connection string
+            string cn_string = Properties.Settings.Default.dbTasksConnectionString;
+
+            //connection
+            SqlConnection cn_connection = new SqlConnection(cn_string);
+
+            if (cn_connection.State != ConnectionState.Open)
+                cn_connection.Open();
+
+            //input from form
+            string taskTitle = titleTextBox.Text;
+            string taskDescription = descriptionTextBox.Text;
+
+            string sql_Text = "INSERT INTO tbl_Tasks (Title, Description) VALUES('" + taskTitle + "','" + taskDescription + "')";
+
+            SqlCommand cmd_Command = new SqlCommand(sql_Text, cn_connection);
+            cmd_Command.ExecuteNonQuery();
+
+            cn_connection.Close();
+
+            //reload
+            todoList.Clear();
+            load_Tasks();
+
+        }
+
+        private void delete_Task()
+        {
+            //connection string
+            string cn_string = Properties.Settings.Default.dbTasksConnectionString;
+
+            //connection
+            SqlConnection cn_connection = new SqlConnection(cn_string);
+
+            if (cn_connection.State != ConnectionState.Open)
+                cn_connection.Open();
+
+            //selected row
+            DataRowView row = todolistView.CurrentRow.DataBoundItem as DataRowView;
+            string IDTask = row["ID"].ToString();
+
+            string sql_Text = "DELETE FROM tbl_Tasks WHERE (ID = " + IDTask + ")";
+
+            SqlCommand cmd_Command = new SqlCommand(sql_Text, cn_connection);
+            cmd_Command.ExecuteNonQuery();
+
+            cn_connection.Close();
+
+            //reload
+            todoList.Clear();
+            load_Tasks();
+        }
+
+        private void edit_Task()
+        {
+            //connection string
+            string cn_string = Properties.Settings.Default.dbTasksConnectionString;
+
+            //connection
+            SqlConnection cn_connection = new SqlConnection(cn_string);
+
+            if (cn_connection.State != ConnectionState.Open)
+                cn_connection.Open();
+
+            //input data
+            string taskTitle = titleTextBox.Text;
+            string taskDescription = descriptionTextBox.Text;
+
+            //selected row
+            DataRowView row = todolistView.CurrentRow.DataBoundItem as DataRowView;
+            string IDTask = row["ID"].ToString();
+
+            string sql_Text = "UPDATE tbl_Tasks SET Title = '" + taskTitle + "', Description = '" + taskDescription + "' WHERE Id = " + IDTask;
+
+            SqlCommand cmd_Command = new SqlCommand(sql_Text, cn_connection);
+            cmd_Command.ExecuteNonQuery();
+
+            cn_connection.Close();
+
+            //reload
+            todoList.Clear();
+            load_Tasks();
+        }
+
+        #endregion /Methods
+
     }
 
 }
